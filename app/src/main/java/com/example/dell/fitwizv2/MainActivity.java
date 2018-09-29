fdeb82f76a02fa743fa0e02a043770f41bb096b2
spi-sample <38002517+spi-sample@users.noreply.github.com>
2018-09-29 19:39:22 +0530

    Initial commit

---
+++ app/src/main/java/com/example/dell/fitwizv2/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.dell.fitwizv2;
+
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        drawerLayout = findViewById(R.id.my_drawer);
+        navigationView = findViewById(R.id.nav_view);
+        textView = findViewById(R.id.textView);
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                menuItem.setChecked(true);
+                textView.setText(menuItem.getTitle());
+                drawerLayout.closeDrawers();
+                return true;
+            }
+        });
+    }
+}
