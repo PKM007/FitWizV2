a046c03b5ece5adfdd56eecc7a7f51fd8a575a8a
spi-sample <38002517+spi-sample@users.noreply.github.com>
2018-10-02 03:47:24 +0530

    Login Firebase

--- app/src/main/java/com/example/dell/fitwizv2/fitness.java
+++ app/src/main/java/com/example/dell/fitwizv2/fitness.java
@@ -9,7 +9,7 @@ import com.squareup.picasso.Picasso;
 
 public class fitness extends AppCompatActivity {
 
-    ImageView squadImage,crunchImage,pushupImage,jumpingjackImage;
+    ImageView squadImage,crunchImage,pushupImage,jumpingjackImage,glutebridgeImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,10 +19,12 @@ public class fitness extends AppCompatActivity {
         crunchImage=findViewById(R.id.crunchImage);
         pushupImage=findViewById(R.id.pushupImage);
         jumpingjackImage=findViewById(R.id.jumpingjackImage);
+        glutebridgeImage=findViewById(R.id.glutebridgeImage);
 
         Picasso.with(getApplicationContext()).load("https://bodyformfitness.com/wp-content/uploads/2016/02/squatting-exercises.jpg").into(squadImage);
         Picasso.with(getApplicationContext()).load("http://www.themusclesecrets.com/images/abdominal-crunches-end.png").into(crunchImage);
         Picasso.with(getApplicationContext()).load("https://static.vecteezy.com/system/resources/previews/000/162/096/non_2x/man-doing-push-up-vector-illustration.jpg").into(pushupImage);
         Picasso.with(getApplicationContext()).load("https://cdn.instructables.com/FLV/BN83/I8SLTHTQ/FLVBN83I8SLTHTQ.LARGE.jpg").into(jumpingjackImage);
+        Picasso.with(getApplicationContext()).load("http://cdn1.coachmag.co.uk/sites/coachmag/files/styles/insert_main_wide_image/public/images/dir_31/mens_fitness_15769.jpg?itok=ZMvl3V6P").into(glutebridgeImage);
     }
 }
