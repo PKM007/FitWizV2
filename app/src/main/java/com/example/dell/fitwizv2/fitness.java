ecbe8c81b44d98273ebec9b798c74e95b7a091ff
spi-sample <38002517+spi-sample@users.noreply.github.com>
2018-10-02 03:06:55 +0530

    Login Firebase

--- app/src/main/java/com/example/dell/fitwizv2/fitness.java
+++ app/src/main/java/com/example/dell/fitwizv2/fitness.java
@@ -9,7 +9,7 @@ import com.squareup.picasso.Picasso;
 
 public class fitness extends AppCompatActivity {
 
-    ImageView squadImage,crunchImage;
+    ImageView squadImage,crunchImage,pushupImage,jumpingjackImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,9 +17,12 @@ public class fitness extends AppCompatActivity {
         setContentView(R.layout.activity_fitness);
         squadImage = findViewById(R.id.squadImage);
         crunchImage=findViewById(R.id.crunchImage);
+        pushupImage=findViewById(R.id.pushupImage);
+        jumpingjackImage=findViewById(R.id.jumpingjackImage);
 
         Picasso.with(getApplicationContext()).load("https://bodyformfitness.com/wp-content/uploads/2016/02/squatting-exercises.jpg").into(squadImage);
         Picasso.with(getApplicationContext()).load("http://www.themusclesecrets.com/images/abdominal-crunches-end.png").into(crunchImage);
-
+        Picasso.with(getApplicationContext()).load("https://static.vecteezy.com/system/resources/previews/000/162/096/non_2x/man-doing-push-up-vector-illustration.jpg").into(pushupImage);
+        Picasso.with(getApplicationContext()).load("https://cdn.instructables.com/FLV/BN83/I8SLTHTQ/FLVBN83I8SLTHTQ.LARGE.jpg").into(jumpingjackImage);
     }
 }
