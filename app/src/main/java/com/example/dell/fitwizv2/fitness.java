bc97234b528ca24622ae765f5f78d151c1390e67
spi-sample <38002517+spi-sample@users.noreply.github.com>
2018-10-02 03:53:44 +0530

    Login Firebase

--- app/src/main/java/com/example/dell/fitwizv2/fitness.java
+++ app/src/main/java/com/example/dell/fitwizv2/fitness.java
@@ -1,7 +1,11 @@
 package com.example.dell.fitwizv2;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.ImageView;
 import java.lang.Object;
 
@@ -10,11 +14,15 @@ import com.squareup.picasso.Picasso;
 public class fitness extends AppCompatActivity {
 
     ImageView squadImage,crunchImage,pushupImage,jumpingjackImage,glutebridgeImage;
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fitness);
+        drawerLayout = findViewById(R.id.my_drawer_signed);
+        navigationView = findViewById(R.id.nav_view);
         squadImage = findViewById(R.id.squadImage);
         crunchImage=findViewById(R.id.crunchImage);
         pushupImage=findViewById(R.id.pushupImage);
@@ -26,5 +34,15 @@ public class fitness extends AppCompatActivity {
         Picasso.with(getApplicationContext()).load("https://static.vecteezy.com/system/resources/previews/000/162/096/non_2x/man-doing-push-up-vector-illustration.jpg").into(pushupImage);
         Picasso.with(getApplicationContext()).load("https://cdn.instructables.com/FLV/BN83/I8SLTHTQ/FLVBN83I8SLTHTQ.LARGE.jpg").into(jumpingjackImage);
         Picasso.with(getApplicationContext()).load("http://cdn1.coachmag.co.uk/sites/coachmag/files/styles/insert_main_wide_image/public/images/dir_31/mens_fitness_15769.jpg?itok=ZMvl3V6P").into(glutebridgeImage);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener()
+
+        {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                menuItem.setChecked(true);
+                drawerLayout.closeDrawers();
+                return true;
+            }
+        });
     }
 }
